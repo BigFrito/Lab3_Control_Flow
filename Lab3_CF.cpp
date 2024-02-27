014fc304e71bdf7b34b0a9b9f0847cee0b5d4670
Isaac R <ruizisaac278@gmail.com>
2024-02-27 15:26:50 -0800

    Checked file to make sure it could be executed

---
+++ Lab3_CF.cpp
@@ -0,0 +1,24 @@
+/***
+ * {Lab 3 - learning how to use boolean expressions, if/else statements, and loops}
+***/
+
+#include <iostream>
+#include <cstdlib>
+
+int main () {
+    int x = 0;
+    int y = 5;
+
+    while(x<y){
+        printf("%i\n",x);
+        x++;
+    }
+    printf("Looped through 5 times\n");
+
+int();
+    if(x>y){
+        printf("x is greater than y/n");
+    }else{
+        printf("x is less than or equal to y/n");
+    }
+}
